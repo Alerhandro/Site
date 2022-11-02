0f134ae7d5e9a2334fc4bd1aba501016bae944e6
Byo <alerhandrojp@gmail.com>
2022-11-01 23:07:11 -0400

    segund commit

---
+++ Input.js
@@ -0,0 +1,20 @@
+function receber1(){
+    var um = document.getElementById("1").value;
+    console.log(um);
+    document.getElementById("um").innerHTML = um;
+}
+function receber2(){
+    var dois = document.getElementById("2").value;
+    console.log(dois);
+    document.getElementById("dois").innerHTML = dois;
+}
+function receber3(){
+    var tres = document.getElementById("3").value;
+    console.log(tres);
+    document.getElementById("tres").innerHTML = tres;
+}
+function receber4(){
+    var quatro = document.getElementById("4").value;
+    console.log(quatro);
+    document.getElementById("quatro").innerHTML = quatro;
+}
